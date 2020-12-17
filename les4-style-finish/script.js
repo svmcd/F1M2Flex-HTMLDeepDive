eb31e2c5c27e6d26b7a6d1a548a3333afd7d32fe
svmcd <spolat0750@gmail.com>
2020-12-17 11:51:12 +0100

    foto slider af

---
+++ les4-style-finish/script.js
@@ -0,0 +1,45 @@
+
+let fotonummer, fotobreedte, aantalfotos, fotoslider;
+
+function setupFotoslider(){
+    //alert("lol")
+
+    fotonummer = 0;
+    fotobreedte = 300;
+
+    fotoslider = document.getElementById('fotoslider');
+    let volgendeKnop = document.getElementById('volgende');
+    let vorigeKnop = document.getElementById('vorige');
+
+    aantalfotos = fotoslider.children.length;
+    console.log(aantalfotos)
+
+    volgendeKnop.addEventListener("click", volgendeFoto);
+    vorigeKnop.addEventListener("click", vorigeFoto);
+}
+
+function volgendeFoto(){
+    fotonummer += 1;
+
+    if (fotonummer === aantalfotos) {
+        fotonummer = 0;
+    }
+
+    let afstandNaarLinks = -1 * (fotonummer * fotobreedte)
+
+    fotoslider.style.left = afstandNaarLinks + "px";
+}
+
+function vorigeFoto(){
+    fotonummer -= 1;
+
+    if (fotonummer < 0) {
+        fotonummer = aantalFotos -1;
+    }
+
+    let afstandNaarLinks = -1 * (fotonummer * fotobreedte)
+
+    fotoslider.style.left = afstandNaarLinks + "px";
+}
+
+window.addEventListener('DOMContentLoaded', setupFotoslider)
